7ce5e7a29aceb7701a5bf98fd2c70680bff43f79
Alison-Cruz <150868902+Alison-Cruz@users.noreply.github.com>
2023-11-15 11:42:21 -0300

    Feat/repository base (#1)

---
+++ src/Core/Contexts/MongoContext.cs
@@ -0,0 +1,10 @@
+﻿using Core.Contexts.Interfaces;
+
+namespace Core.Contexts
+{
+    public class MongoContext : IMongoContext
+    {
+        public string DatabaseName { get; set; } = string.Empty;
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}

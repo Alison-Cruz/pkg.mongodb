40f92937fcae27683d7089728708ce5412105bfb
Alison-Cruz <alisonscruz12@gmail.com>
2023-11-15 11:22:58 -0300

    feat: create attribute

---
+++ src/Core/Attributes/BsonCollectionAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Attributes
+{
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}

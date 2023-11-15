cc10be62ee2f6fdad2bbb1b62e9d2e65eaf770ff
Alison-Cruz <alisonscruz12@gmail.com>
2023-11-15 11:23:17 -0300

    feat: add entities

---
+++ src/Core/Entities/Document.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Interfaces;
+using MongoDB.Bson;
+
+namespace Core.Entities
+{
+    public abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+}
